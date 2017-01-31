b3b6e2ee02216e9de85395a7cc92a4f14a101d71
Antonio <Antonio@Antonios-MacBook-Pro.local>
2017-01-31 13:43:41 +0200

    First change to the project + Deploy to Heroku

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const port = process.env.PORT || 3000;
 var app = express();
 
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.get('/', (req, res) => {
-    res.send('Hello Express!');
+    res.render('home', {});
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects', {
+        "pageTitle": "Projects"
+    })
 });
 
 app.listen(port, () => {console.log(`Server started on port ${port}`);})
